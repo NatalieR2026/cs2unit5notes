70e27e204242295396bb5f4dedc79d88d115eead
NatalieR2026 <natalie.radin2026@gbwl.org>
2024-12-10 13:54:51 +0000

    12/10

--- student.java
+++ student.java
@@ -3,19 +3,35 @@ Class names like Student are always capitalized!
 Object-Oriented Classes are like BLUEPRINTS
 (in another Java class, you create instances from this blueprint)
  */
-public class Student {
-    // 1. INSTANCE VARIABLES (attributes/data/fields)
-    // think of what this object "needs to know"
-    // must be PRIVATE ("encapsulation")
-    private String name; // "a Student has a name"
-    private int gradYear; 
-    private String email; 
-    private boolean sports;
-    private double gpa;
+    public class Student {
+        // 1. INSTANCE VARIABLES (attributes/data/fields)
+        // think of what this object "needs to know"
+        // must be PRIVATE ("encapsulation")
+        private String fName; // "a Student has a name"
+        private String lName;
+        private int gradYear; 
+        private String email;
+    
+        // 2. CONSTRUCTORS (manuals for setting up an object)
+        
+        // DEFAULT constructor (user did not provide data
+        // Job: initialize instance variables to default values
+        public Student() {
+            fName = "First";
+            lName = "Last";
+            gradYear = 2037;
+            email = fName + "." + lName + gradYear + "@gbwl.org"
+        }
 
-    // 2. CONSTRUCTORS (manuals for setting up an object)
+        // Constructor with PARAMETERS (user provided key data)
+        // Job: initialize instance variables to PROVIDED values
+        public Student(String initFName, String initLName, int initGradYear) {
+            fName = initFName;
+            lName = initLName;
+            gradYear = initGradYear;
+            email = fName + "." + lName + gradYear + "@gbwl.org"
+        }
 
-
-    // 3. METHODS (behaviors associated w/ the object type)
+        // 3. METHODS (behaviors associated w/ the object type)
 
 }
